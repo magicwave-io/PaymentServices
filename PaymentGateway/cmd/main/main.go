2f5a37c5a0b0501510a9d33554db005e58c4f886
a.gorulev <a.gorulev@kernelics.com>
2024-08-09 17:00:20 +0000

    magicwave.io

---
+++ PaymentGateway/cmd/main/main.go
@@ -0,0 +1,44 @@
+package main
+
+import "C"
+import (
+	"fmt"
+	"os"
+	"runtime"
+
+	"paidpiper.com/payment-gateway/log"
+
+	"paidpiper.com/payment-gateway/common"
+	"paidpiper.com/payment-gateway/config"
+	"paidpiper.com/payment-gateway/version"
+
+	"paidpiper.com/payment-gateway/serviceNode"
+)
+
+func main() {
+	if len(os.Args) == 2 && (os.Args[1] == "version" || os.Args[1] == "--version") {
+		fmt.Printf("payment_gateway %v, build %v", version.Version(), version.BuildDate())
+		return
+	}
+	stop := make(chan os.Signal, 1)
+	log.Infof("payment_gateway %v, built %v ", version.Version(), version.BuildDate())
+	config, err := config.ParseConfig()
+	log.Info("Port: ", config.Port)
+	if err != nil {
+		log.Errorf("get config error: %v", err)
+		<-stop
+		return
+	}
+
+	tracerShutdownFunc := common.InitGlobalTracer(config.JaegerConfig)
+	defer tracerShutdownFunc()
+	runtime.GOMAXPROCS(config.MaxConcurrency)
+	runtime.NumGoroutine()
+	serverShutdown, err := serviceNode.RunHttpServer(config)
+	if err != nil {
+		log.Panicf("Error starting serviceNode: %v", err)
+	} else {
+		defer serverShutdown()
+	}
+	<-stop
+}
---
+++ PaymentGateway/controllers/httpGatewayController.go
@@ -0,0 +1,83 @@
+package controllers
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+
+	"paidpiper.com/payment-gateway/log"
+
+	"paidpiper.com/payment-gateway/models"
+	"paidpiper.com/payment-gateway/node/local"
+)
+
+type HttpGatewayController struct {
+	local.LocalPPNode
+}
+
+func NewHttpGatewayController(n local.LocalPPNode) *HttpGatewayController {
+	return &HttpGatewayController{
+		n,
+	}
+}
+
+func (g *HttpGatewayController) HttpProcessResponse(w http.ResponseWriter, r *http.Request) {
+
+	response := &models.UtilityResponseFixModel{}
+	data, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Errorf("Error read request body: %s", err.Error())
+	}
+	log.Infof("Http process response %v", string(data))
+	err = json.NewDecoder(bytes.NewReader(data)).Decode(response)
+
+	if err != nil {
+		log.Errorf("Error decoding request: %s", err.Error())
+		log.Tracef("Body: %v", string(data))
+
+		Respond(w, MessageWithStatus(http.StatusBadRequest, "Invalid request"))
+		return
+	}
+	res := models.NewShapelessProcessCommandResponse(response)
+	err = g.ProcessResponse(context.Background(), res)
+	if err != nil {
+		log.Errorf("Error processing response: %s", err.Error())
+		Respond(w, MessageWithStatus(http.StatusConflict, err.Error()))
+		return
+	}
+
+}
+
+func (g *HttpGatewayController) HttpProcessPayment(w http.ResponseWriter, r *http.Request) {
+	ctx, span := spanFromRequest(r, "ProcessPayment")
+	defer span.End()
+
+	request := &models.ProcessPaymentRequest{}
+	body, err := ioutil.ReadAll(r.Body)
+	jsonString := string(body[:])
+
+	err = json.NewDecoder(strings.NewReader(jsonString)).Decode(request)
+
+	if err != nil {
+		log.Errorf("Error decoding payment request: %s", err.Error())
+		Respond(w, MessageWithStatus(http.StatusBadRequest, fmt.Sprintf("Bad request:%v", err)))
+		return
+	}
+	res, err := g.ProcessPayment(ctx, request)
+	if err != nil {
+		log.Errorf("Error processing payment request: %s", err.Error())
+		Respond(w, MessageWithStatus(http.StatusBadRequest, err.Error()))
+		return
+	}
+	if res != nil {
+		log.Infof("Payment processing complete (session %s)", res.SessionId)
+		Respond(w, MessageWithData(http.StatusCreated, res))
+		return
+	}
+	Respond(w, MessageWithStatus(http.StatusOK, "Payment processing completed"))
+
+}
