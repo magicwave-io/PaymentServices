52896965f6d883f5f36f1e677cf393af31f702a2
a.gorulev <a.gorulev@kernelics.com>
2024-12-10 06:00:05 +0000

    magicwave.io

---
+++ PaymentGateway/tests/sequences.go
@@ -0,0 +1,49 @@
+package tests
+
+import (
+	"context"
+
+	"paidpiper.com/payment-gateway/log"
+
+	"github.com/stretchr/testify/assert"
+	"paidpiper.com/payment-gateway/models"
+)
+
+type Sequencer struct {
+	testSetup *TestSetup
+	assert    *assert.Assertions
+	ctx       context.Context
+}
+
+func CreateSequencer(testSetup *TestSetup, assert *assert.Assertions, ctx context.Context) Sequencer {
+	sq := Sequencer{
+		testSetup: testSetup,
+		assert:    assert,
+		ctx:       ctx,
+	}
+
+	return sq
+
+}
+
+func (sq Sequencer) PerformPayment(sourceSeed string, destinationSeed string, commodityAmount uint32) (*models.ProcessPaymentAccepted, *models.PaymentRequest, error) {
+
+	pr, err := sq.testSetup.NewPaymentRequest(sq.ctx, destinationSeed, commodityAmount)
+	sq.assert.NoError(err)
+
+	if err != nil {
+		log.Printf("Error: %v", err)
+		return nil, nil, err
+	}
+
+	result, err := sq.testSetup.ProcessPayment(sq.ctx, sourceSeed, pr)
+	sq.assert.NoError(err)
+
+	if err != nil {
+		log.Printf("Error: %v", err)
+		return nil, nil, err
+	}
+
+	return result, pr, nil
+
+}
