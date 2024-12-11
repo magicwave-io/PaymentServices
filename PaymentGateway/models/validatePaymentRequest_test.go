455dbf19a6612c13c1dab38a99d74528c32f599c
a.gorulev <a.gorulev@kernelics.com>
2024-12-11 07:00:42 +0000

    magicwave.io

---
+++ PaymentGateway/models/validatePaymentRequest_test.go
@@ -0,0 +1,27 @@
+package models
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMarshalValidatePaymentRequest(t *testing.T) {
+	assert := assert.New(t)
+	m := &ValidatePaymentRequest{
+		PaymentRequest: PaymentRequest{},
+	}
+	bs, err := json.Marshal(m)
+	assert.Nil(err)
+
+	unm := &ValidatePaymentRequest{}
+	err = json.Unmarshal(bs, unm)
+	assert.Nil(err)
+
+	if !cmp.Equal(*unm, *m) {
+		t.Error("Not equal")
+	}
+
+}
