304bb5a954fc66fc6aa9468be9ab50338d6a9595
a.gorulev <a.gorulev@kernelics.com>
2025-01-28 10:00:35 +0000

    magicwave.io

---
+++ PaymentGateway/node/local/paymentregestry/database/entity/transaction.go
@@ -0,0 +1,17 @@
+package entity
+
+import "time"
+
+type DbTransaction struct {
+	Id                        int
+	Sequence                  int64
+	TransactionSourceAddress  string
+	ReferenceAmountIn         int
+	AmountOut                 int
+	XDR                       string
+	PaymentSourceAddress      string
+	PaymentDestinationAddress string
+	StellarNetworkToken       string
+	ServiceSessionId          string
+	Date                      time.Time
+}
